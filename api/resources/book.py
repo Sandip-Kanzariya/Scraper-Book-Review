dd6130363c1c435adb396c1f966f74960d6583f0
Sandip-Kanzariya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2024-07-01 16:21:09 +0530

    Render postgreSQL added

--- api/resources/book.py
+++ api/resources/book.py
@@ -4,7 +4,7 @@ from flask_restful import Resource
 from bs4 import BeautifulSoup
 import requests
 from extensions import db
-
+from models.books import Book
 
 # http://127.0.0.1:5050/books/
 class BookList(Resource):
@@ -54,11 +54,32 @@ class BookList(Resource):
                     book_title = book_details[0].find('h3', class_='booktitle').a.text
                     book_title_list.append(book_title)
                     
+                storebook = Book(
+                    title=book_title,
+                    author=book_author,
+                    book_link=book_link,
+                    cover_link=book_cover_link
+                )
+                db.session.add(storebook)
+                db.session.commit()
 
-                results = {}
-                results['Title'] = book_title_list
-                results['Author'] = book_author_list
-                results['BookLink'] = book_link_list
-                results['CoverLink'] = book_cover_link_list
-        
-        return results
+    
+        results = {
+            'Title': book_title_list, 
+            'Author': book_author_list, 
+            'BookLink': book_link_list,
+            'CoverLink': book_cover_link_list}
+
+
+        response = []
+
+        for i in range(len(book_title_list)):
+            response.append({
+                'Title': book_title_list[i],
+                'Author': book_author_list[i],
+                'BookLink': book_link_list[i],
+                'CoverLink': book_cover_link_list[i]
+            }) 
+
+
+        return response
--- models/books.py
+++ models/books.py
@@ -6,4 +6,4 @@ class Book(db.Model):
     title = db.Column(db.String(300))
     author = db.Column(db.String(200))
     book_link = db.Column(db.String(500))
-    book_image_url = db.Column(db.String(500))
+    cover_link = db.Column(db.String(500))
